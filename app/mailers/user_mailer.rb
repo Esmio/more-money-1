dbb4443d0be6c33757a8462a1467763238ca3ac1
dongxiaoming <hy44jt@126.com>
2020-11-10 23:35:37 +0800

    邮箱注册

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    @url = ''
+    @url = 'http://weixin.qq.com'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   #
   # config.action_mailer.perform_caching = false
-  config.actioin_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
