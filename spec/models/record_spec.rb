0167031b6f15b02f62423a01aeaca50f32b9da06
dongxiaoming <hy44jt@126.com>
2020-11-24 01:01:11 +0800

    创建记录

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe Record, type: :model do
   it 'amount is required' do
     record = Record.create notes: 'xxx', category: 'outgoings'
-    p record.errors
     expect(record.errors.details[:amount][0][:error]).to eq(:blank)
   end
   it 'category is required' do
     record = Record.create notes: 'xxx', amount: 10000
-    p record.errors
     expect(record.errors.details[:category][0][:error]).to eq(:blank)
+  end
+  it 'category 只能是 outgoings 或者 income' do
+    expect {
+      Record.create notes: 'xxx', amount: 10000, category: 'outgoing'
+    }.to raise_error(ArgumentError)
   end
 end
