ea937dba20dbc0160ee8a7517159a35e281850a0
dongxiaoming <hy44jt@126.com>
2020-11-25 01:47:10 +0800

    完成 record 文档

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require 'custom_error'
 class ApplicationController < ActionController::API
   rescue_from CustomError::MustSignInError, with: :render_must_sign_in
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
   def must_sign_in
     if current_user.nil?
       raise CustomError::MustSignInError
@@ -22,5 +23,9 @@ class ApplicationController < ActionController::API
 
   def render_must_sign_in
     render status: :unauthorized
+  end
+
+  def render_not_found
+    render status: :not_found
   end
 end
