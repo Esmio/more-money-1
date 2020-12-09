930392db4d4ba272e4dd74ceea410efd55b76b78
dongxiaoming <hy44jt@126.com>
2020-12-10 01:20:24 +0800

    添加更新 record 的文档

--- spec/acceptance/record_spec.rb
+++ spec/acceptance/record_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "Records" do
-  let(:record){ Record.create! amount: 10000, category: 'income' }
-  let(:id){record.id}
+  let(:record) { Record.create! amount: 10000, category: 'income' }
+  let(:id) { record.id }
+  let(:amount) { 10000 }
+  let(:category) { 'income' }
+  let(:notes) { '中奖' }
   post "/records" do
-    parameter :amount , '金额', type: :integer, require: true
-    parameter :category, '类型:1 outgoings |2 income', type: :string, require: true
+    parameter :amount, '金额', type: :integer, required: true
+    parameter :category, '类型:1 outgoings |2 income', type: :string, required: true
     parameter :notes, '备注', type: :string
     example "创建记账" do
       sign_in
@@ -26,7 +29,7 @@ resource "Records" do
   get "/records" do
     parameter :page, '页码', type: :integer
 
-    let(:page) {1}
+    let(:page) { 1 }
 
     (1..11).to_a.map do
       Record.create! amount: 10000, category: 'income'
@@ -42,6 +45,17 @@ resource "Records" do
     example '获取一个记录' do
       sign_in
       do_request
+      expect(status).to eq 200
+    end
+  end
+
+  patch "/records/:id" do
+    parameter :amount, '金额', type: :integer
+    parameter :category, '类型:1 outgoings |2 income', type: :string
+    parameter :notes, '备注', type: :string
+    example '更新一个记录' do
+      sign_in
+      do_request amount: 9900
       expect(status).to eq 200
     end
   end
